1233ef9d49b0dafcaf743cb28aac7f342465c597
Berkant Karaca <berkantkaracatr@gmail.com>
2025-07-16 18:24:59 +0300

    update

--- include/banner.php
+++ include/banner.php
@@ -4,10 +4,10 @@
             <div class="col-lg-6 p-2"><img class="avatar img-fluid mt-2 ms-5" src="images/avatar.webp" width="450" height="500" alt="Berkant Karaca - Jr. Backend Developer ve Bilgisayar Mühendisi Profil Fotoğrafı" fetchpriority="high" /></div>
             <div class="col-lg-6">
                 <div class="mt-5">
-                    <p class="lead text-uppercase mb-1">Merhaba!</p>
-                    <h1 class="intro-title marker" data-aos="fade-left" data-aos-delay="50">Ben, Berkant Karaca</h1>
-                    <p class="lead fw-normal mt-3" data-aos="fade-up" data-aos-delay="100">Jr. Backend Developer & Bilgisayar Mühendisi</p>
-                    <div class="social-nav" data-aos="fade-up" data-aos-delay="200">
+                    <p class="lead text-uppercase mb-1" data-aos="fade-right" >Merhaba!</p>
+                    <h1 class="intro-title marker" data-aos="fade-left" >Ben, Berkant Karaca</h1>
+                    <p class="lead fw-normal mt-3" data-aos="fade-up" >Jr. Backend Developer & Bilgisayar Mühendisi</p>
+                    <div class="social-nav" data-aos="fade-up">
                         <nav role="navigation">
                             <ul class="nav justify-content-left">
                                 <li class="nav-item">
@@ -25,7 +25,7 @@
                             </ul>
                         </nav>
                     </div>
-                    <div class="mt-3" data-aos="fade-up" data-aos-delay="200">
+                    <div class="mt-3" data-aos="fade-up">
                         <a class="btn btn-primary shadow-sm mt-1 hover-effect" href="#contact">İletişime Geç <i
                                 class="fas fa-arrow-right"></i></a>
                     </div>
