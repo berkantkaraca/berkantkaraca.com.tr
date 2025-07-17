dd784c6d001d67393bdafc5a9e26576b6236cb0a
Berkant Karaca <berkantkaracatr@gmail.com>
2025-07-17 10:11:11 +0300

    update language

---
+++ index copy.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+
+<?php include 'include/head.php'; ?>
+
+<body id="top">
+
+  <?php include 'include/header.php'; ?>
+
+  <div class="page-content">
+    <div id="content">
+
+      <?php include 'include/banner.php'; ?>
+  
+      <?php include 'include/about.php'; ?>
+
+      <?php include 'include/education.php'; ?>
+  
+      <?php include 'include/skills.php'; ?>
+  
+      <?php include 'include/experience.php'; ?>
+  
+      <?php include 'include/portfolio.php'; ?>
+
+      <?php include 'include/certificate.php'; ?>
+  
+      <?php include 'include/contact.php'; ?>
+
+    </div>
+  </div>
+  
+  <?php include 'include/footer.php'; ?>
+  
+  <?php include 'include/scripts.php'; ?>
+  
+</body>
+</html>
